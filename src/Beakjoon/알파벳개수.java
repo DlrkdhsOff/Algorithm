85b71928f25271a6cd3e0f1f131b0a88d0b9b9ab
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-01-13 22:30:25 +0900

    백준 : 알페벳 개수

---
+++ src/Beakjoon/알파벳개수.java
@@ -0,0 +1,25 @@
+package Beakjoon;
+
+import java.io.*;
+
+public class 알파벳개수 {
+  public static void main(String[] args) throws IOException{
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    char[] arr = bf.readLine().toCharArray();
+
+    int[] result = new int[26];
+
+    for (char c : arr) {
+      result[c - 97]++;
+    }
+
+    for (int i : result) {
+      bw.write(i + " ");
+    }
+    bw.write("\n");
+    bw.flush();
+  }
+
+}
