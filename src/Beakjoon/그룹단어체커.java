b2a18ef0879284c3fa107cf638e5f816ea4ef420
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-01-20 22:31:14 +0900

    백준 : 그룹 단어 체커

---
+++ src/Beakjoon/그룹단어체커.java
@@ -0,0 +1,48 @@
+package Beakjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 그룹단어체커 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int result = 0;
+    for (int i = 0; i < N; i++) {
+      String input = br.readLine();
+
+      if (isVisited(input)) {
+        result++;
+      }
+
+    }
+    bw.write(result + "\n");
+    bw.flush();
+  }
+
+  public static boolean isVisited(String word) {
+    boolean[] visited = new boolean[26];
+    char prev = word.charAt(0);
+
+    visited[prev - 'a'] = true;
+    for (int i = 1; i < word.length(); i++) {
+      char ch = word.charAt(i);
+
+      if (visited[ch - 'a'] && prev != ch) {
+        return false;
+      }
+
+      visited[ch - 'a'] = true;
+      prev = ch;
+    }
+
+    return true;
+  }
+
+}
