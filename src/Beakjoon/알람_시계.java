0be1b20e32504790f2b0206633ae5c2a358d7459
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-01-24 21:55:00 +0900

    백준 : 알람시계

---
+++ src/Beakjoon/알람_시계.java
@@ -0,0 +1,24 @@
+package Beakjoon;
+
+import java.util.Scanner;
+
+public class 알람_시계 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int hour = scan.nextInt();
+    int min = scan.nextInt();
+    scan.nextLine();
+
+    if (min >= 45) {
+      System.out.printf("%d %d" , hour, min - 45);
+    } else {
+      if (hour == 0) {
+        System.out.printf("%d %d" , 23, min += (60 - 45));
+      } else {
+        System.out.printf("%d %d" , hour -= 1, min += (60 - 45));
+      }
+
+    }
+  }
+
+}
