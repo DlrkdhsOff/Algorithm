b77755b0f0ac0f743ccbdbca9591d1bb28e2d08f
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-05 22:16:44 +0900

    백준 : 알고리즘 문제

---
+++ src/programmers/level_1/문자열_다루기_기본.java
@@ -0,0 +1,22 @@
+package programmers.level_1;
+
+public class 문자열_다루기_기본 {
+
+  public static void main(String[] args) {
+    System.out.println(solution("1234"));
+  }
+
+
+  public static boolean solution(String s) {
+    if(s.length() == 4 || s.length() == 6) {
+      String[] arr = s.split("[^A-z]");
+
+      if(arr.length == 0) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+}
