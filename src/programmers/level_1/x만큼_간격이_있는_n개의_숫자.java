848a73f177bf395967194cd2b914b1614fc9b9b4
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-13 17:56:20 +0900

    프로그래머스 : Level 1

--- src/programmers/level_1/x만큼_간격이_있는_n개의_숫자.java
+++ src/programmers/level_1/x만큼_간격이_있는_n개의_숫자.java
@@ -1,21 +1,20 @@
 package programmers.level_1;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class x만큼_간격이_있는_n개의_숫자 {
 
   public static void main(String[] args) {
-    System.out.println(solution(-4, 2));
+    System.out.println(Arrays.toString(solution(2, 5)));
   }
 
-  public static List<Long> solution(int x, int n) {
-    List<Long> result = new ArrayList<>();
+  public static long[] solution(int x, int n) {
+    long[] answer = new long[n];
 
-    Long data = 0L;
-    for(int i = 0; i < n; i++) {
-      result.add(data += x);
+    for(int i = 1; i <= n; i++) {
+      answer[i - 1] = (long) i * x;
     }
 
-    return result;
+    return answer;
   }
 }
