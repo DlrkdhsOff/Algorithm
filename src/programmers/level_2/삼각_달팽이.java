64b6a77ef3c0c25156ed8703e87263e6067a4fa0
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-24 19:36:29 +0900

    프로그래머스 : Level 1

---
+++ src/programmers/level_2/삼각_달팽이.java
@@ -0,0 +1,47 @@
+package programmers.level_2;
+
+import java.util.Arrays;
+
+public class 삼각_달팽이 {
+
+  public static void main(String[] args) {
+    System.out.println("result: " + Arrays.toString(solution(6)));
+  }
+  public static int[] solution(int n) {
+    int[] result = new int[n * (n + 1) / 2];
+    int[][] arr = new int[n][n];
+
+    int row = -1;
+    int col = 0;
+    int num = 1;
+
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+
+        switch (i % 3) {
+          case 0:
+            arr[++row][col] = num++;
+            break;
+          case 1:
+            arr[row][++col] = num++;
+            break;
+          case 2:
+            arr[--row][--col] = num++;
+            break;
+        }
+      }
+    }
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (arr[i][j] != 0) {
+          result[index++] = arr[i][j];
+        }
+      }
+    }
+
+    return result;
+  }
+}
