f70a02a8a5ca1b1f8b86c7a556e6a701bdf8735a
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-04-03 01:12:14 +0900

    프로그래머스 : pcce 기출문제 1

---
+++ src/programmers/pcce/기출문제_1_문자출력.java
@@ -0,0 +1,12 @@
+package programmers.pcce;
+
+public class 기출문제_1_문자출력 {
+
+  public static void main(String[] args) {
+    String message = "Let's go!";
+
+    System.out.println("3\n2\n1");
+    System.out.println(message);
+  }
+
+}
